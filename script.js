238e05519dad1faa8537df9b122dfec340276a71
Viv Hayles <vivhayles@gmail.com>
2024-10-15 11:17:51 +0100

    added flower background image, styled coding projects

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ fetch('codingprojects.json')
         
         data.project.forEach((project) => {
             codingProjects.innerHTML += `
-                <div class="codingPortfolio">
-                    <h3>${project.name}</h3>
+                <div class="codingPortfolio m-4 p-4 grid-container border-4 border-violet-300 rounded border-solid">
+                    <h3 class = "text-lg">${project.name}</h3>
                     <p class="desc">${project.description}</p>
-                    <img class="image" src="${project.img}" alt="blank placeholder image"/>
-                    <a href="${project.githublink}" target="_blank">View on GitHub</a>
-                    <a href="${project.livelink}" target="_blank">Live Demo</a>
+                    <img class="image flex justify-center py-4" src="${project.img}" alt="blank placeholder image"/>
+                    <a class = "pe-4" href="${project.githublink}" target="_blank">View on GitHub</a>
+                    <a class = "pe-4" href="${project.livelink}" target="_blank">Live Demo</a>
                 </div>
             `
         })

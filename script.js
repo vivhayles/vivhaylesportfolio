6a37e577ff7e4c28873108772c24ccdaebd9d0c8
Viv Hayles <vivhayles@gmail.com>
2024-10-15 13:57:30 +0100

    Made the jump links in nav bar functional, added download CV button to nav bar, changed background picture.

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ fetch('codingprojects.json')
         data.project.forEach((project) => {
             codingProjects.innerHTML += `
                 <div class="codingPortfolio m-4 p-4 grid-container border-4 border-violet-300 rounded border-solid">
-                    <h3 class = "text-lg">${project.name}</h3>
+                    <h3 class = "text-lg font-medium ">${project.name}</h3>
                     <p class="desc">${project.description}</p>
-                    <img class="image flex justify-center py-4" src="${project.img}" alt="blank placeholder image"/>
-                    <a class = "pe-4" href="${project.githublink}" target="_blank">View on GitHub</a>
-                    <a class = "pe-4" href="${project.livelink}" target="_blank">Live Demo</a>
+                    <img class="image py-4 mx-auto" src="${project.img}" alt="blank placeholder image"/>
+                    <a class = "pe-4 font-medium hover:text-violet-500" href="${project.githublink}" target="_blank">View on GitHub</a>
+                    <a class = "pe-4 font-medium hover:text-violet-500" href="${project.livelink}" target="_blank">Live Demo</a>
                 </div>
             `
         })
 
     })
+
+    const menuButton = document.querySelector(".menu-button");
+const menuLinks = document.querySelector('nav .links')
+
+menuButton.addEventListener("click", toggleMobileMenu) 
+
+function toggleMobileMenu() {
+    menuLinks.classList.toggle('open')
+}
